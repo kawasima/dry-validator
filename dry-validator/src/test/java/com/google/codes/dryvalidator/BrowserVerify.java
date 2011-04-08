86643e6aaa2528582079df158b1e6bce46cf7586
kawasima <kawasima1016@gmail.com>
2011-04-08 00:57:40 +0000

    ブラウザテストは一旦手動でやるように修正
    git-svn-id: http://dry-validator.googlecode.com/svn/trunk@23 e0b8b0c3-8fa8-58b7-3822-3b281b5a1e72

--- dry-validator/src/test/java/com/google/codes/dryvalidator/BrowserVerify.java
+++ dry-validator/src/test/java/com/google/codes/dryvalidator/BrowserVerify.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
-public class BrowserTest {
+public class BrowserVerify {
 	@BeforeClass
 	public static void ウェブサーバを起動する() throws Exception{
 		Server server = new Server();
